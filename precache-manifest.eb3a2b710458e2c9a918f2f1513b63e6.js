0f6d116e422dfba1a942c9c16dec74f6127750b4
jojojojo1322 <miz123@naver.com>
2019-03-18 11:23:42 +0900

    Updates

---
+++ precache-manifest.eb3a2b710458e2c9a918f2f1513b63e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7952d9d4b252ca1c580e",
+    "url": "/movie_app/static/js/runtime~main.e6fc6960.js"
+  },
+  {
+    "revision": "7a42d83dafcad0ece16d",
+    "url": "/movie_app/static/js/main.ffa47e64.chunk.js"
+  },
+  {
+    "revision": "79876ddfb540893b229e",
+    "url": "/movie_app/static/js/2.28db7df0.chunk.js"
+  },
+  {
+    "revision": "7a42d83dafcad0ece16d",
+    "url": "/movie_app/static/css/main.d9faa3ba.chunk.css"
+  },
+  {
+    "revision": "2651996c8b512f001debb5fa314e9260",
+    "url": "/movie_app/index.html"
+  }
+];
